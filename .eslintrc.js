a489ecec9dc623651188b2a495e892b92140e98e
indominuShot <luang193@gmail.com>
2022-03-20 16:38:16 -0300

    style: config styleguide

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,14 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
 };
