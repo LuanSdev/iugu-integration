e0b74de2d376ed8d8873dd75ab2639d71019a020
indominuShot <luang193@gmail.com>
2022-03-29 16:53:05 -0300

    feat: add mock

---
+++ __mocks__/iugu.js
@@ -0,0 +1,12 @@
+module.exports = {
+  createPaymentTokenCalls: 0,
+  createPaymentTokenData: null,
+  createPaymentTokenResponse: { id: 'any-id' },
+
+  createPaymentToken(data, response) {
+    this.createPaymentTokenCalls++;
+    this.data = data;
+
+    return this.response;
+  },
+};
