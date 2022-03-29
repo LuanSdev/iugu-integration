c691aee8c428bb5239a5e1a76f22fcdd8fd5aa6c
indominuShot <luang193@gmail.com>
2022-03-29 16:17:15 -0300

    fix: fix import

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,5 +1,5 @@
 export * from './iugu-bank-slip';
-export * from './iugu-credit_card_transaction';
+export * from './iugu-credit-card-transaction';
 export * from './iugu-invoice-create-request';
 export * from './iugu-invoice-create-response';
 export * from './iugu-invoice-item';
