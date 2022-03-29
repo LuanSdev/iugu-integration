90d79bd35bfcbdeb0007135d100e456097b68116
indominuShot <luang193@gmail.com>
2022-03-29 20:58:58 -0300

    feat: add type

---
+++ src/@types/iugu-customer.ts
@@ -0,0 +1,16 @@
+export type IuguCustomer = {
+  cc_emails?: string;
+  city?: string;
+  complement?: string;
+  cpf_cnpj?: string;
+  district?: string;
+  email: string;
+  name: string;
+  notes?: string;
+  number?: string;
+  phone_prefix?: number;
+  phone?: number;
+  state?: string;
+  street?: string;
+  zip_code?: string;
+};
